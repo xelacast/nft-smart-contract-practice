03ca575e6007029950967fa06dbfbf94af06ea62
Alexander Castillo <jhuiicebox@gmail.com>
2022-02-14 19:37:55 -0700

    Optimized DemoDapp contract

--- contracts/DemoDapp.sol
+++ contracts/DemoDapp.sol
@@ -10,95 +10,25 @@ import "hardhat/console.sol";
 contract DemoDapp is ERC1155, Ownable {
     uint256 constant receiptTokenId = 0;
     uint256 receiptTotalSupply = 3000;
-    uint256 fruitTokenId = 1;
-    uint256 fourInOneTokenId = 100000;
+    uint256 fruitTokenId = 1; // first season will go to 4000
+    uint256 fourInOneTokenId = 100001; // first season will go to 101000
 
-    uint256 batchSupply = 1000;
     uint256 giveaways = 250;
+    uint256 batchSupply = 1000 - giveaways;
 
     uint256[] batchMintAmmount = [1, 1, 1, 1, 1, 3];
     uint256[] batchIdHolder = [0, 0, 0, 0, 0, 0];
 
-    string private _uri;
-
     event IncreaseReceiptSupply(address _sender, uint256 _supply);
 
-    mapping(uint256 => string) private tokenToUri;
-    mapping(address => uint256) private bundleBalance;
-    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private bundleBalance; // stop people from trading out and buying if nfts are traded to another account the account traded to can still buy
+    mapping(uint256 => address) private _owners; // Token to wallet address
 
-    constructor(string memory uri) ERC1155(uri) {
-        _uri = uri;
-        _setUri(receiptTokenId);
-    }
-
-    // this is a hunch that this will populate the nft token name on etherscan
-    // but it could be the meta data that does that for erc1155
-    // need more info.
-
-    // this was removed for a purpose
-    // function name() public view virtual returns (string memory) {
-    //     return _name;
-    // }
-    // this was removed for a purpose
-    // function symbol() public view virtual returns (string memory) {
-    //     return _symbol;
-    // }
-
-    /// @dev Overide _setUri to input the ids into the uris
-    function _setUri(uint256 _tokenId) internal virtual {
-        tokenToUri[_tokenId] = string(
-            abi.encodePacked(_uri, Strings.toString(_tokenId), ".json")
-        );
-        emit URI(_uri, _tokenId);
-    }
-
-    function getUri(uint256 _tokenId)
-        public
-        view
-        virtual
-        returns (string memory)
-    {
-        return tokenToUri[_tokenId];
-        // string(abi.encodePacked(_uri, Strings.toString(_tokenId), ".json"));
-    }
-
-    // I do not have to set these uris nor do i have to map them to a mapping
-    function setBatchUris(uint256[] memory _batchIds)
-        internal
-        returns (uint256[] memory)
-    {
-        // must find a way to pad the uri id with 64 characters
-        tokenToUri[fruitTokenId] = string(
-            abi.encodePacked(_uri, Strings.toString(fruitTokenId), ".json")
-        );
-        _batchIds[0] = fruitTokenId;
-        fruitTokenId++;
-        tokenToUri[fruitTokenId] = string(
-            abi.encodePacked(_uri, Strings.toString(fruitTokenId), ".json")
-        );
-        _batchIds[1] = fruitTokenId;
-        fruitTokenId++;
-        tokenToUri[fruitTokenId] = string(
-            abi.encodePacked(_uri, Strings.toString(fruitTokenId), ".json")
-        );
-        _batchIds[2] = fruitTokenId;
-        fruitTokenId++;
-        tokenToUri[fruitTokenId] = string(
-            abi.encodePacked(_uri, Strings.toString(fruitTokenId), ".json")
-        );
-        _batchIds[3] = fruitTokenId;
-        fruitTokenId++;
-
-        _batchIds[4] = fourInOneTokenId;
-        fourInOneTokenId++;
-        _batchIds[5] = receiptTokenId;
-        return _batchIds;
-    }
+    constructor(string memory uri) ERC1155(uri) {}
 
     function mintBundle() external payable {
         require(
-            msg.value >= 0.05 ether,
+            msg.value >= 0.08 ether,
             "Not enough ether in account to transact."
         );
         require(
@@ -106,7 +36,11 @@ contract DemoDapp is ERC1155, Ownable {
             "Cannot purchase more than one batch mint per wallet"
         );
         require(batchSupply > 0, "All Batches have been minted");
-        require(fruitTokenId < 4001, "All tokens have been given away");
+        // require(fruitTokenId < 4001, "All tokens have been given away");
+        require(
+            msg.sender == tx.origin,
+            "Contract calls cannot mint our supply"
+        );
 
         // set token ids that are going to be minted
         uint256[] memory idHolder = batchIdHolder;
@@ -122,15 +56,36 @@ contract DemoDapp is ERC1155, Ownable {
         fourInOneTokenId++;
         idHolder[5] = receiptTokenId;
 
-        // idHolder = setBatchUris(idHolder);
-        batchSupply--;
-        // check with different logic using balanceOf ERC1155 function
         bundleBalance[msg.sender]++;
-        receiptTotalSupply = receiptTotalSupply - 3; // do i need this if im working with batch supply?
 
         _mintBatch(msg.sender, idHolder, batchMintAmmount, "");
 
-        // console.log("Hash of token id", hashing);
+        receiptTotalSupply = receiptTotalSupply - 3;
+        batchSupply--;
+    }
+
+    /// @notice This givaway will not impact the presale and sale to minting limit of 1
+    // Can i make this a multiple match batch to gift multiple people at once?
+    function giveawayMintBatch(address _to) external onlyOwner {
+        require(giveaways > 0, "Out of Giveaways.");
+        uint256[] memory idHolder = batchIdHolder;
+        idHolder[0] = fruitTokenId;
+        fruitTokenId++;
+        idHolder[1] = fruitTokenId;
+        fruitTokenId++;
+        idHolder[2] = fruitTokenId;
+        fruitTokenId++;
+        idHolder[3] = fruitTokenId;
+        fruitTokenId++;
+        idHolder[4] = fourInOneTokenId;
+        fourInOneTokenId++;
+        idHolder[5] = receiptTokenId;
+
+        _mintBatch(_to, idHolder, batchMintAmmount, "");
+
+        receiptTotalSupply = receiptTotalSupply - 3;
+        giveaways--;
+        batchSupply--;
     }
 
     function increaseReceiptSupply(uint256 _supply) external onlyOwner {
@@ -142,11 +97,11 @@ contract DemoDapp is ERC1155, Ownable {
         return bundleBalance[msg.sender];
     }
 
-    function getBatchSupply() public view returns (uint256) {
+    function getBatchSupply() public view onlyOwner returns (uint256) {
         return batchSupply;
     }
 
-    function getReceiptSupply() public view returns (uint256) {
+    function getReceiptSupply() public view onlyOwner returns (uint256) {
         return receiptTotalSupply;
     }
 
@@ -154,7 +109,8 @@ contract DemoDapp is ERC1155, Ownable {
         return address(this).balance;
     }
 
-    function withdrawBalance(address payable _to) external onlyOwner {
-        _to.transfer(address(this).balance);
+    function withdrawBalance() external onlyOwner {
+        require(address(this).balance > 0, "No ether in contract");
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 
 async function main() {
   let Contract, contract, owner, addr1, addr2;
-  const URI = "https://cuteeFruitee.api/tokens/"
+  const URI = "https://cuteeFruitee.api/tokens/{id}.json"
   // these are removed for erc1155 token standard for duplication of data
   // const name = "DemoNFT";
   // const symbol = "DNFT";
@@ -16,21 +16,44 @@ async function main() {
   console.log("Contract deployed with id: ", contract.address);
 
   // basic test of getting the uri
-  console.log("URI is: ", await contract.getUri(0));
+  console.log("URI is: ", await contract.uri(0));
 
   await contract.connect(addr1).mintBundle({value: ethers.utils.parseEther("0.08")})
   console.log(await addr1.getBalance())
 
-  console.log("Balance of token id 0 with address: ", await contract.balanceOf(addr1.address, 0))
+  console.log("Balance of token id 0: ", await contract.balanceOf(addr1.address, 0))
   console.log("Balance of token id 1: ", await contract.balanceOf(addr1.address, 1))
   console.log("Balance of token id 2: ", await contract.balanceOf(addr1.address, 2))
   console.log("Balance of token id 3: ", await contract.balanceOf(addr1.address, 3))
   console.log("Balance of token id 4: ", await contract.balanceOf(addr1.address, 4))
   console.log("Balance of token id 5: ", await contract.balanceOf(addr1.address, 5))
-  console.log("Balance of token id 100000: ", await contract.balanceOf(addr1.address, 100000))
+  console.log("Balance of token id 100001: ", await contract.balanceOf(addr1.address, 100001))
 
   console.log("Balance of contract: ", ethers.utils.formatEther(await contract.getBalanceOfContract()));
   console.log(await contract.connect(addr1).getBundleBalance());
+  console.log(await contract.getBatchSupply());
+  console.log(await contract.getReceiptSupply());
+
+  console.log()
+
+  await contract.connect(owner).giveawayMintBatch(addr2.address);
+  console.log(await addr2.getBalance());
+  await contract.connect(addr2).mintBundle({value: ethers.utils.parseEther("0.08")});
+  // try {
+  //   await contract.connect(addr1).giveawayMintBatch(addr1.address);
+  // } catch(error) {
+  //   console.log("Cant access giveaways if you are not the owner.")
+  // }
+
+  console.log("Balance of token id 0: ", await contract.balanceOf(addr1.address, 0))
+  console.log("Balance of token id 5: ", await contract.balanceOf(addr2.address, 5))
+  console.log("Balance of token id 6: ", await contract.balanceOf(addr2.address, 6))
+  console.log("Balance of token id 7: ", await contract.balanceOf(addr2.address, 7))
+  console.log("Balance of token id 8: ", await contract.balanceOf(addr2.address, 8))
+  console.log("Balance of token id 9: ", await contract.balanceOf(addr2.address, 9))
+  console.log("Balance of token id 100002: ", await contract.balanceOf(addr2.address, 100002))
+  console.log("Balance of contract: ", ethers.utils.formatEther(await contract.getBalanceOfContract()));
+  console.log(await contract.connect(addr2).getBundleBalance());
   console.log(await contract.getBatchSupply());
   console.log(await contract.getReceiptSupply());
 
